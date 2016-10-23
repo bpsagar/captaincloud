762e148162a6ccfd18743826eeea968cc61ad3c5
Sagar Chakravarthy <bp.sagar@gmail.com>
2016-10-23 12:03:09 +0530

    Adding basic package related files.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='captaincloud',
+    version='1.0.0-alpha',
+    description='',
+    url='https://github.com/bpsagar/captaincloud',
+    author='Sagar Chakravarthy',
+    license='MIT',
+    packages=['captaincloud'],
+    install_requires=[
+        'six'
+    ],
+    zip_safe=False)
