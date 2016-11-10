f49cf4f9d9f534bd2f3a075ebb0581610a30aa11
Sattvik Chakravarthy <sattvik@gmail.com>
2016-11-10 09:30:34 +0530

    Fixing setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name='captaincloud',
@@ -7,7 +7,7 @@ setup(
     url='https://github.com/bpsagar/captaincloud',
     author='Sagar Chakravarthy',
     license='MIT',
-    packages=['captaincloud'],
+    packages=find_packages(exclude=('tests',)),
     install_requires=[
         'six'
     ],
