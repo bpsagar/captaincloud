9f9a068c7c35a6f280a39c9e77192beb629e928c
Sagar Chakravarthy <bp.sagar@gmail.com>
2016-11-15 18:50:05 +0530

    Reorganizing task runner methods.

--- captaincloud/processes/container/task_runner.py
+++ captaincloud/processes/container/task_runner.py
@@ -17,6 +17,14 @@ class TaskRunner(threading.Thread):
         self._is_running = False
         self._tasks = {}
 
+    def get_task(self, run_id):
+        """Returns a task with this run_id"""
+        return self._tasks.get(run_id)
+
+    def set_task(self, run_id, task):
+        """Set a test for this run_id"""
+        self._tasks[run_id] = task
+
     def stop(self):
         """Stop the task runner"""
         self._is_running = False
@@ -29,19 +37,19 @@ class TaskRunner(threading.Thread):
     def add(self, task):
         """Set a RUN_ID to the task and add the task to the queue"""
         task.RUN_ID = str(uuid.uuid4())
+        self.set_task(run_id=task.RUN_ID, task=task)
         self._queue.put(task)
-        self._tasks[task.RUN_ID] = task
         self.set_status(run_id=task.RUN_ID, status=TaskRunner.WAITING)
         return task.RUN_ID
 
     def get_status(self, run_id):
         """Returns the status of a Task"""
-        task = self._tasks.get(run_id)
+        task = self.get_task(run_id=run_id)
         return task.STATUS
 
     def set_status(self, run_id, status):
         """Set status of the task"""
-        task = self._tasks.get(run_id)
+        task = self.get_task(run_id=run_id)
         task.STATUS = status
 
     def get_logger(self, task):
